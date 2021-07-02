78da2aa66a4a0ba5f5b4a6e694475c74c0ea6930
siddhant2424-cmyk <75306546+siddhant2424-cmyk@users.noreply.github.com>
2021-07-02 09:42:43 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,73 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "a cell is 28th from top and 4th from right what is it's cell address",
+    answer: "D28",
+    options: [
+        "F98",
+        "D28",
+        "Y82",
+        "U87",
+        
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is a column range",
+    answer: "the number of cells spread across the column",
+    options: [
+      "the number of cells spread across the column",
+      "the number of cells spread across the row",
+      "the number of cells spread across the first row",
+      "the number of cells spread across the first column",
+    ]
+  },
+    {
+    numb: 3,
+    question: "What are values",
+    answer: "any data containing only special characters",
+    options: [
+        "any data containing only special characters",
+      "Any expressions that begins with an equals",
+      "any data containing all the special characters numbers and alphabets",
+      "The numerical data consisting of only numbers",
+    ]
+  },
+    {
+    numb: 4,
+    question: "what is  16/(4^2) ",
+    answer: "1",
+    options: [
+      "1",
+      "69",
+      "673",
+      "2"
+    ]
+  },
+    {
+    numb: 5,
+    question: "what is a label",
+    answer: "Label is the any text",
+    options: [
+      "Label is the any number",
+      "Label is the any text",
+      "Label is the any formulae",
+      "Label is the any special character"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
