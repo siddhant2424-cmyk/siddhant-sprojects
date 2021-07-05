eb2accc3930bde61209e5e3698db1f7ca6a1766a
siddhant2424-cmyk <75306546+siddhant2424-cmyk@users.noreply.github.com>
2021-07-05 09:35:18 +0530

    Add files via upload

---
+++ question2.js
@@ -0,0 +1,183 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Who was Sudha Chandran",
+    answer: "A dancer",
+    options: [
+        "A dancer",
+        "A singer",
+        "A writer",
+        "A physicist",
+        
+    ]
+  },
+    {
+    numb: 2,
+    question: "what was her main problem in dancing",
+    answer: "her broken leg",
+    options: [
+      "her broken hand",
+      "her broken leg",
+      "her deafness",
+      "her blindness",
+    ]
+  },
+    {
+    numb: 3,
+    question: "How did she hurt her leg",
+    answer: "She hurt her leg in a road accident",
+    options: [
+        "She hurt her while dancing on the stage",
+        "She hurt her leg in a road accident",
+        "She hurt her leg while hiking",
+        "She hurt her leg while playing football",
+    ]
+  },
+  {
+  numb: 4,
+  question: "in which year did her accident occur",
+  answer: "In 1981",
+  options: [
+    "In 1997",
+    "In 1984",
+    "In 1981",
+    "In 1990",
+  ]
+},
+{
+numb: 5,
+question: "what was her inspiration as told my Ms sudha",
+answer: "her failures",
+options: [
+  "her achievements",
+  "her parents",
+  "her dance teacher",
+  "her failures",
+]
+},
+    {
+    numb: 6,
+    question: "Stephen Hawking was a..... ",
+    answer: "physicist",
+    options: [
+        "astrobiologist",
+        "physicist",
+        "Bacteriologist",
+        "guitarist",
+    ]
+  },
+    {
+    numb: 7,
+    question: "in which year did Stephen Hawking left this world",
+    answer: "2018",
+    options: [
+      "2018",
+      "1999",
+      "1997",
+      "2008",
+    ]
+  },
+  {
+    numb: 8,
+    question: "through whcih diorder was Stephen Hawking suffering?",
+    answer: "neurogenic paralysis",
+    options: [
+      "Dementia",
+      "Bipolar disorder",
+      "neurogenic paralysis",
+      "Schizophrenia ",
+    ]
+  },
+  {
+    numb: 9,
+    question: "what was the full name of Stephen Hawking",
+    answer: "Stephen William Hawking",
+    options: [
+      "Stephen adams Hawking",
+      "Stephen William Hawking",
+      "Stephen Hawking jhoe",
+      "Stephen Hawking aderson",
+    ]
+  },
+  {
+    numb: 10,
+    question: "what were the greatest doscoveries of stephen hawking",
+    answer: "Discovery of Black Hole Dynamics and Gravitational Singularity",
+    options: [
+      "Discovery of light bulb and gravity",
+      "Discovery of Black Hole Dynamics and Gravitational Singularity",
+      "Discovery of atom",
+      "Discovery of motor",
+    ]
+  },
+  {
+    numb: 11,
+    question: "through whcih diorder was Helen Keller suffering?",
+    answer: "blindness and deafness",
+    options: [
+      "blindness",
+      "dumbness",
+      "blindness and deafness",
+      "deafness",
+    ]
+  },
+  {
+    numb: 12,
+    question: "how did she lost her sense of hearing and sight",
+    answer: "she lost her sight and hearing after a bout of illness",
+    options: [
+      "she lost her sight and hearing after an accident",
+      "she didn't have sense of hearing and sight from birth",
+      "she lost her sight and hearing in an operation",
+      "she lost her sight and hearing after a bout of illness",,
+    ]
+  },
+  {
+    numb: 13,
+    question: "how did she listen people deaf and blind",
+    answer: "she put her thumb on the speaker's lips and understand him",
+    options: [
+      "she put her thumb on the speaker's lips and understand him",
+      "by using a machine",
+      "the speaker used to wright words on her back",
+      "by seeing the speaker's lips",
+    ]
+  },
+  {
+    numb: 14,
+    question: "Helen Keller was he firt deaf blind person to...",
+    answer: "earn a bachelor's degree",
+    options: [
+      "earn a PHD",
+      "earn a master's degree",
+      "earn a bachelor's degree",
+      "earn a Honors in English",
+    ]
+  },
+  {
+    numb: 15,
+    question: "what was the full name of Helen Keller",
+    answer: "Helen Adams Keller",
+    options: [
+      "Helen Keller wilson",
+      "Helen Adams Keller",
+      "Helen fisk Keller",
+      "Helen williams Keller",
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
